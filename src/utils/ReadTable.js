eef56b96abf5a1e4e456b8ecb1298fbdc07d9604
shreyas0512 <shreyassanthosh362@gmail.com>
2023-09-19 12:59:20 +0530

    worker implemented

--- src/utils/ReadTable.js
+++ src/utils/ReadTable.js
@@ -13,20 +13,18 @@ function tableResults(link) {
         res = resul.data;
         const finalResult = {
           data: res,
-          time: time2 - time,
+          //round to 3 decimal places
+          time: (time2 - time).toFixed(3),
         };
         resolve(finalResult);
       },
+      worker: true,
       error: (error) => {
         console.error("Error parsing CSV:", error);
         reject(error);
       },
     });
   });
-}
-
-function tableResults2(filename) {
-  //parse local file uploaded by user
 }
 
 export default tableResults;
