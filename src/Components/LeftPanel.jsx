5da8c09f960be680da42d60579b1dc7f48a95eef
shreyas0512 <shreyassanthosh362@gmail.com>
2023-09-21 03:24:01 +0530

    table ui bug fix

--- src/Components/LeftPanel.jsx
+++ src/Components/LeftPanel.jsx
@@ -8,7 +8,7 @@ function LeftPanel() {
   return (
     <div className="h-full sm:w-[20vw] w-[91vw] self-center  rounded-tr-md rounded-br-md border-[1px] border-[#C7C5C5] shadow-sm flex flex-col ">
       <TableList />
-      <div className="h-[2px] rounded-lg mx-3 mb-1 w-fill bg-gray-300"></div>
+      <div className="h-[1.5px] rounded-lg mx-3 my-2 w-fill bg-gray-300"></div>
       <QueryList />
     </div>
   );
